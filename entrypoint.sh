7fc69907fe0017a818c2a9684c2a0e4216ca78c3
Dwi Didit Prasetiyo <didit@dwidi.com>
2024-06-08 11:26:18 +0700

    Edit entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 # Run Alembic migrations
 alembic upgrade head
 
-# Start the application
-uvicorn main:app --host 0.0.0.0 --port 8000
+# Start the FastAPI application
+exec uvicorn main:app --host 0.0.0.0 --port 8000
